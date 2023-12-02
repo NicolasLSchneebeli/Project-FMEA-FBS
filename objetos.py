6ad1ce74e8045ba8bbeb0904a37c817952ac8c5b
Nicolas Schneebeli <118636061+NicolasLSchneebeli@users.noreply.github.com>
2023-12-02 16:09:32 +0100

    Added a dictionary to understand the process to simulate better -Probably need some improvements

--- objetos.py
+++ objetos.py
@@ -33,10 +33,10 @@ class Propriety():
         self.component=component
         self.risk=risk
         self.state= True 
+        self.source=[]
         self.component.addAttribute(self)
         self.link=[]
         self.FailureMode={}
-        # print(f"Attribute created named {self.name} part of {self.component.name}")   
    
     #Just to confirm if is ok
     def getComponent(self):
@@ -45,12 +45,17 @@ class Propriety():
     #Add time when this component failed!      
     def addFailedTick(self,tick,data,origin):
         self.FailedTick= tick
-        data.loc[len(data)]=(self.FailedTick,self.name,self.component.name,origin)
+        self.source=origin
+        if origin.name != self.name:
+            data.loc[len(data)]=(self.FailedTick,self.name,self.component.name,f"{origin.name} from {origin.component}")
+        else:
+            data.loc[len(data)]=(self.FailedTick,self.name,self.component.name,'self')
 
+        
     
     #Create via ENTITIY NAMED LINK!!
-    def addLinkToAttribute(self, attr2, risk,time_to_infect,status):
-        self.link.append([self, attr2,time_to_infect, risk,status])  
+    def addLink(self,att2,risk,time):
+        print('Teste')
     
     #LIST OF LINKS COMPOSED OF ATT1, ATT2, RISK, COOLDOWN TIME, STATUS
     def getLinks(self):
@@ -58,36 +63,21 @@ class Propriety():
             print(f"There is a link between {link[0].name} (FROM: {link[0].component.name}) and {link[1].name} (FROM: {link[1].component.name}) with risk {link[3]}") 
         return self.link
     
-    #Change our entity state to FAILED while being infected by somebody else
+    #Change our entity state to FAILED
     def getInfected(self,t,origin,df):
         self.state=False
-        self.addFailedTick(tick=t,data=df,origin=origin)           
-    
+        self.addFailedTick(tick=t,data=df,origin=origin)  
+        
+        
+    '''For FMEA Table. Perhaps adding a dictionary
+    like linking to EXCESS or LOSS, idk yet.
+    '''     
     def addFailureMode(self,Names,Risks):
         if sum(Risks)!=100:
-            print("ATTENTION: Sum of probabilities diferent that 100%")
-            pass #MAKE SOMEHOW TO CHECK AND INTERRUPT
+            print("Failure mode sum of probabilities diferent that 100%")
         for i in range(len(Names)):
-            self.FailureMode[Names[i]]= Risks[i] 
-#NOTE: CREATING EACH LINK FOR EACH ATTRIBUTE
-'''How to create other kinds of links such as Temperatures/Distance etc. Don't see how this would impact the simul in general...?'''      
-class Link():
-    
-    def __init__(self,time,risk,attribute1, attribute2) -> None:  
-        self.risk= risk
-        self.time_to_infect=time
-        self.stat= True
-        self.status= True   
-        
-        self.attribute1= attribute1
-        self.attribute2=attribute2
-        attribute1.addLinkToAttribute(attr2=self.attribute2,risk=self.risk,time_to_infect= self.time_to_infect,status=self.stat)
-        attribute2.addLinkToAttribute(attr2=self.attribute1, risk=self.risk,time_to_infect= self.time_to_infect,status=self.stat) 
-                 
-        print(f"{self.__class__.__name__} created between {attribute1.name} and {attribute2.name}")
-            
-#Perhaps adding as a Bigger Attribute/Component might work! 
-''' How to implement their links?'''             
+            self.FailureMode[Names[i]]= Risks[i]
+
 class Behaviour():
     
     def __init__(self,name) -> None:
