4346c75fb81aa53fc9aab85e7ba9cac564cc525e
Nicolas Schneebeli <118636061+NicolasLSchneebeli@users.noreply.github.com>
2023-12-02 14:01:50 +0100

    Redone link matrix + still redoable the simulation, proceed to data analysis

--- function.py
+++ function.py
@@ -3,9 +3,13 @@ import datetime as dt
 import numpy as np
 import random as rd
 import time
-
+import os 
+import glob
 #Funciton to run the simulation 
-def State_machine(components, tick, behaviour):
+def State_machine(components, behaviour,link_matrix,attrs): 
+    tick=0
+    matrix=np.copy(link_matrix)
+    """MAKE A WAY TO RESTART THE LINK MATRIX EACH INTERACTION!!!!"""
     for beh in behaviour:
         beh.state= True
     for component in components:
@@ -34,42 +38,45 @@ def State_machine(components, tick, behaviour):
                             print(f"{propriety.name} from {propriety.component.name} *just* failed on tick: {tick}")
                             failures=list(propriety.FailureMode.keys())
                             chances=[propriety.FailureMode[i] for i in failures]
-                            if chances == []:
-                                propriety.addFailedTick(tick=tick,data=df,origin='self')
-                            else:
-                                result=rd.choices(failures,chances,k=1)[0]
-                                propriety.addFailedTick(tick=tick,data=df,origin=f'self ({result})')
+                            # if chances == []:
+                            propriety.addFailedTick(tick=tick,data=df,origin=propriety)
+                            # else:
+                            #     result=rd.choices(failures,chances,k=1)[0]
+                            #     propriety.addFailedTick(tick=tick,data=df,origin=f'self ({result})')
 
                             
                         else:
                             print(f"{propriety.name} from {propriety.component.name} is {result}")
 
                             
-                    #When the state of the attribute is false, meaning that it has failed.      
+                        """When the state of the attribute is false,
+                        meaning that it has failed."""      
                     else:
-                        print(f"{propriety.name} from {propriety.component.name} failed on tick {propriety.FailedTick}, which is {tick-propriety.FailedTick} ticks ago")
-                        #Getting the links of the each attribute.
-                        links_of_propriety = np.asarray(propriety.link)
-                        for i in range(len(links_of_propriety)):
-                            if links_of_propriety[i][4]==False:
-                                pass
-                            
-                            '''Check if it is time yet for him to failed! (Right now just time, cause of implementation, 
+                        '''Check if it is time yet for him to failed! (Right now just time, cause of implementation, 
                             perhaps it is a good alternative since is easy to determine unlike temperature for exemple, 
                             which I imagine taking a lot of the memory of the PC. )'''
-                            
-                            if tick - propriety.FailedTick  == links_of_propriety[i][2] and links_of_propriety[i][4]== True:
-                                risk= links_of_propriety[i][3]
-                                links_of_propriety[i][4]== False
-                                weights = [100-risk,risk]
-                                resultinf= rd.choices(states,weights,k=1)[0]
-                                if resultinf == "Failed":
-                                    links_of_propriety[i][1].getInfected(t=tick,df=df,origin=f'{links_of_propriety[i][0].name} from {links_of_propriety[i][0].component.name}')
-                                    links_of_propriety[i][4]=False 
-                                    '''Doesnt change for the other link.
-                                    I dont know how to acess the other and 
-                                    change its state too, dont see as priority either.'''
-                                    print(f"{links_of_propriety[i][1].name} from {links_of_propriety[i][1].component.name} was infected by {propriety.name}")
+                        print(f"{propriety.name} from {propriety.component.name} failed on tick {propriety.FailedTick}, which is {tick-propriety.FailedTick} ticks ago")
+                        
+                        """Getting the links of the each attribute."""
+                        i = attrs.index(propriety)
+                        for j in range(matrix.shape[1]):
+                            '''Relating the columns and getting the links active for this attribute
+                            REVISE THIS CONDITION UNDER HERE!!'''
+                            if sum(matrix[i,j]) > 0:
+                                if tick -propriety.FailedTick >= matrix[i, j, 1]:
+                                    risk= matrix[i, j, 0]
+                                    states_ = ["Working", "Failed"]
+                                    weights = [100-risk,risk]
+                                    resultinf= rd.choices(states_,weights,k=1)[0]
+                                    if resultinf == "Failed":
+                                        attrs[j].getInfected(t=tick,df=df,origin=propriety)
+                                        matrix[i,j]=[0,0]
+                                        matrix[j,i]=[0,0]
+                                        print(f"{attrs[j].name} from {attrs[j].component.name} was infected by {propriety.name}")
+                                else:
+                                    pass
+                            else:
+                                pass
                              
             print(f'----------------------------Behaviour-------------------------------')
             for beh in behaviour:
@@ -78,6 +85,7 @@ def State_machine(components, tick, behaviour):
             time.sleep(3)
         else:
             toSave(df=df,behaviour=behaviour,start_time=stt)
+
     else:
         print(erro)
 #NOTE: Remove the error for append method is a good way to improve the quality of the display and the results
@@ -92,16 +100,12 @@ def check(component):
         return False, erro
     for comp in component:
         if comp.name.lower() in component_name:
-            erro= ('Two or more components with the same name! \n Try adding numbers. F.e. Motor1 and Motor2 or more specific names ')
+            erro= ('Two or more components with the same name! \n Try adding numbers. F.e. Component1 and Component2 instead of Component and Component')
             return False,erro
         component_name.add(comp.name.lower())
-        
         if comp.attribute == []:
-                erro=('There is a component(s) without attribute!')
+                erro=('There is a component(s) without attribute. Please add an attribute')
                 return False,erro
-        # if comp.attribute.link == []:
-        #         erro=('There is a attribute without link!')
-        #         return False,erro
     else:
         erro= None
         return True,erro
@@ -119,4 +123,43 @@ def toSave(df,behaviour,start_time):
     print('===================================DONE================================================')
     print(f'CSV saved in: Simulation/Simulation_{dt.datetime.now().day}_{dt.datetime.now().month}_{dt.datetime.now().year}_{dt.datetime.now().hour}_{dt.datetime.now().minute}.csv')
     print(f'Time to complete {time.time()- start_time} seconds')
-    #cant save in a excel file. Dont know why yet, but its not priority i think. 
+    """cant save in a excel file. Dont know why yet, but its not priority i think.""" 
+
+
+
+''''RIGHT NOW I HAVE TO DO IT MANUALLY'''
+def createMatrix(components):
+    attrs=[]
+    for component in components:
+        for propriety in component.attribute:
+            attrs.append(propriety) 
+            
+    return np.zeros((len(attrs),len(attrs),2),dtype=int),attrs
+
+def createLink(matrix,attribute_list,attribute1,attribute2,risk,time):
+    """Finding the index related to the attributes"""   
+    i = attribute_list.index(attribute1)
+    j = attribute_list.index(attribute2)
+    
+    '''Adding/Updating the matrix for the values for the link with RISK, TIME'''
+    matrix[i, j] = [risk,time] 
+    matrix[j, i] = [risk,time]
+    
+    
+    
+
+
+
+'''
+YET TO BE IMPLEMENTED PROPERLY
+'''  
+def analysis(path):
+    path=os.path.join(path)
+    csv_files = glob.glob(os.path.join(path, "*.csv")) 
+    dfs=[]
+    for f in csv_files:
+        df_ = pd.read_csv(f)
+        dfs.append(df_)
+    df = pd.concat(dfs, ignore_index=True)
+    
+    return df
